153efbbbe0b999ae73db658989f2c963eec66de8
yuchenweng7@gmail.com <yuchenweng7@gmail.com>
2024-03-10 12:05:24 +0800

    add error msg

--- routers/users.js
+++ routers/users.js
@@ -9,6 +9,7 @@ router.get("/", async (req, res, next) => {
     const result = await pool.query("SELECT * from users limit 100;");
     res.send({ post: result.rows, status: "success" });
   } catch (error) {
+    console.log(error);
     res.send({ status: "fail" });
   }
 });
