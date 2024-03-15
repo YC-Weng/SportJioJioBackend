0a6343d4bd6e2cce2fa737718aaa0d33cdf689d9
yuchenweng7@gmail.com <yuchenweng7@gmail.com>
2024-03-15 12:44:48 +0800

    line bot

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const http = require("http");
 const https = require("https");
 const fs = require("fs");
-const line = require("@line/bot-sdk");
+const linebot = require("linebot");
 
 const postsRouter = require("./routers/posts");
 const usersRouter = require("./routers/users");
@@ -14,14 +14,14 @@ var privateKey = fs.readFileSync("sslcert/sportjiojio.key", "utf8");
 var certificate = fs.readFileSync("sslcert/sportjiojio_site.crt", "utf8");
 
 const credentials = { key: privateKey, cert: certificate };
-const line_config = {
+
+const app = express();
+const bot = linebot({
+  channelId: "2004077067",
   channelAccessToken:
     "7zt2lErH/nyFGDOH7nINASCLFu1bvtdWKhSpl/eqCwRmRER0BxU5+S5acla5TVSfenjhPfShrECO1aFp/OL77OXDeXQk+qDCMA/T7x/tnfeqZyoewjj75CfKTWow8MzBsfMzUW5xSMeDiR7DRc896QdB04t89/1O/w1cDnyilFU=",
   channelSecret: "1b18be1ef9e541dfe7157e71ab3ba78b",
-};
-
-const app = express();
-const line_client = new line.messagingApi.MessagingApiClient(line_config);
+});
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
@@ -37,35 +37,6 @@ app.use((req, res, next) => {
 app.use("/posts", postsRouter);
 app.use("/users", usersRouter);
 app.use("/groups", groupsRouter);
-
-app.post("linecallback", line.middleware(line_config), (req, res, next) => {
-  console.log(req.body);
-  Promise.all(req.body.events.map(handleEvent))
-    .then((result) => {
-      console.log(result);
-      res.json(result);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).end();
-    });
-});
-
-function handleEvent(event) {
-  if (event.type !== "message" || event.message.type !== "text") {
-    // ignore non-text-message event
-    return Promise.resolve(null);
-  }
-
-  // create a echoing text message
-  const echo = {
-    replyToken: event.replyToken,
-    messages: [{ type: "text", text: event.message.text }],
-  };
-
-  // use reply API
-  return client.replyMessage(echo);
-}
 
 app.get("/", (req, res) => {
   res.send("Welcome to SportJioJio Backend!!");
@@ -86,13 +57,31 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-// app.listen(3000, () => console.log("Example app is listening on port 3000."));
-var httpServer = http.createServer(app);
+bot.on("message", function (event) {
+  // event.message.text是使用者傳給bot的訊息
+  // 準備要回傳的內容
+  var replyMsg = `Hello你剛才說的是:${event.message.text}`;
+  // 透過event.reply(要回傳的訊息)方法將訊息回傳給使用者
+  event
+    .reply(replyMsg)
+    .then(function (data) {
+      // 當訊息成功回傳後的處理
+    })
+    .catch(function (error) {
+      // 當訊息回傳失敗後的處理
+    });
+});
+
+// var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 
-httpServer.listen(3001, () =>
-  console.log("HTTP Backend server is listening on port 3001")
-);
+// httpServer.listen(3001, () =>
+//   console.log("HTTP Backend server is listening on port 3001")
+// );
 httpsServer.listen(3000, () =>
   console.log("HTTPS Backend server is listening on port 3000")
 );
+
+bot.listen("/linecallback", 3001, () => {
+  console.log("line bot is listening on port 3001");
+});
