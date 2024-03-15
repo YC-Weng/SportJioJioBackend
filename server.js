e7c49ab3a8a347a2e401cb64c958ebc0b73dc466
yuchenweng7@gmail.com <yuchenweng7@gmail.com>
2024-03-15 12:51:22 +0800

    no http server

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const path = require("path");
 const http = require("http");
 const https = require("https");
 const fs = require("fs");
-const linebot = require("linebot");
 
 const postsRouter = require("./routers/posts");
 const usersRouter = require("./routers/users");
@@ -16,12 +15,6 @@ var certificate = fs.readFileSync("sslcert/sportjiojio_site.crt", "utf8");
 const credentials = { key: privateKey, cert: certificate };
 
 const app = express();
-const bot = linebot({
-  channelId: "2004077067",
-  channelAccessToken:
-    "7zt2lErH/nyFGDOH7nINASCLFu1bvtdWKhSpl/eqCwRmRER0BxU5+S5acla5TVSfenjhPfShrECO1aFp/OL77OXDeXQk+qDCMA/T7x/tnfeqZyoewjj75CfKTWow8MzBsfMzUW5xSMeDiR7DRc896QdB04t89/1O/w1cDnyilFU=",
-  channelSecret: "1b18be1ef9e541dfe7157e71ab3ba78b",
-});
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
@@ -30,7 +23,8 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   console.log("Time: ", Date.now());
-  console.log(`${req.method} ${req.url} ${req.ip}`);
+  console.log(`${req.method} ${req.url} ${req.ip} 
+    body: ${req.body}`);
   next();
 });
 
@@ -57,21 +51,6 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-bot.on("message", function (event) {
-  // event.message.text是使用者傳給bot的訊息
-  // 準備要回傳的內容
-  var replyMsg = `Hello你剛才說的是:${event.message.text}`;
-  // 透過event.reply(要回傳的訊息)方法將訊息回傳給使用者
-  event
-    .reply(replyMsg)
-    .then(function (data) {
-      // 當訊息成功回傳後的處理
-    })
-    .catch(function (error) {
-      // 當訊息回傳失敗後的處理
-    });
-});
-
 // var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 
@@ -81,7 +60,3 @@ var httpsServer = https.createServer(credentials, app);
 httpsServer.listen(3000, () =>
   console.log("HTTPS Backend server is listening on port 3000")
 );
-
-bot.listen("/linecallback", 3001, () => {
-  console.log("line bot is listening on port 3001");
-});
