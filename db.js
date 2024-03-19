bb9ed86f609c4f6a70e7468ff3a3ce78239070e4
yuchenweng7@gmail.com <yuchenweng7@gmail.com>
2024-03-19 20:02:55 +0800

    add env variables

--- db.js
+++ db.js
@@ -2,19 +2,19 @@ const pg = require("pg");
 const fs = require("fs");
 
 const config_testing = {
-  user: "sjjstaff",
-  database: "jjdb",
-  password: "sportjiojio",
-  port: 5432,
-  host: "localhost",
+  user: process.env.LDB_user,
+  database: process.env.LDB_database,
+  password: process.env.LDB_password,
+  port: process.env.LDB_port,
+  host: process.env.LDB_host,
 };
 
 const config_developing = {
-  user: "doadmin",
-  database: "jjdb",
-  password: "AVNS_-GWAu0hX_V8M32qsPQj",
-  port: 25060,
-  host: "jiojio-do-user-15809025-0.c.db.ondigitalocean.com",
+  user: process.env.RMDB_user,
+  database: process.env.RMDB_database,
+  password: process.env.RMDB_password,
+  port: process.env.RMDB_port,
+  host: process.env.RMDB_host,
   ssl: {
     rejectUnauthorized: false,
     ca: fs.readFileSync("./sslcert/sportjiojio_site.crt"),
