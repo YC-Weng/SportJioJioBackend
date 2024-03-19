9da328bd0c4bb64fd141ff51f7156cd35558f543
yuchenweng7@gmail.com <yuchenweng7@gmail.com>
2024-03-19 14:59:10 +0800

    add DO RMDB

--- db.js
+++ db.js
@@ -1,11 +1,21 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
+const config_testing = {
   user: "sjjstaff",
   database: "jjdb",
   password: "sportjiojio",
   port: 5432,
   host: "localhost",
-});
+};
+
+const config_developing = {
+  user: "doadmin",
+  database: "jjdb",
+  password: "AVNS_-GWAu0hX_V8M32qsPQj",
+  port: 25060,
+  host: "jiojio-do-user-15809025-0.c.db.ondigitalocean.com",
+};
+
+const pool = new Pool(config_developing);
 
 module.exports = { pool };
