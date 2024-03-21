e81bd4d160d000138e3b7aa6a762b532f5bba2e5
yuchenweng7@gmail.com <yuchenweng7@gmail.com>
2024-03-21 21:04:08 +0800

    fix bugs

--- routers/users.js
+++ routers/users.js
@@ -57,6 +57,13 @@ router.post("/create", async (req, res, next) => {
     const { userName, userId, userPicUrl } = req.body;
     if (userName == null) res.send({ status: "fail" });
     else {
+      console.log(
+        `INSERT into users (id, name${
+          userPicUrl == null ? ", pic_url" : ""
+        }) values ('${userId}', '${userName}'${
+          userPicUrl == null ? `, '${userPicUrl}'` : ""
+        })`
+      );
       const result = await pool.query(
         `INSERT into users (id, name${
           userPicUrl == null ? ", pic_url" : ""
--- server.js
+++ server.js
@@ -24,6 +24,11 @@ app.set("view engine", "jade");
 
 app.use(cors());
 app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 app.use((req, res, next) => {
   console.log("Time: ", Date.now());
