dd74a439821b5f5f3643bdc81932d11b5b59facc
yuchenweng7@gmail.com <yuchenweng7@gmail.com>
2024-03-22 02:06:00 +0800

    line router

--- routers/line.js
+++ routers/line.js
@@ -25,8 +25,39 @@ router.post("/", async (req, res, next) => {
         replyToken: replyToken,
         messages: [
           {
-            type: "text",
-            text: "Hello, user",
+            type: "template",
+            altText: "This is a buttons template",
+            template: {
+              type: "buttons",
+              thumbnailImageUrl: "https://example.com/bot/images/image.jpg",
+              imageAspectRatio: "rectangle",
+              imageSize: "cover",
+              imageBackgroundColor: "#FFFFFF",
+              title: "Menu",
+              text: "Please select",
+              defaultAction: {
+                type: "uri",
+                label: "View detail",
+                uri: "http://example.com/page/123",
+              },
+              actions: [
+                {
+                  type: "postback",
+                  label: "Buy",
+                  data: "action=buy&itemid=123",
+                },
+                {
+                  type: "postback",
+                  label: "Add to cart",
+                  data: "action=add&itemid=123",
+                },
+                {
+                  type: "uri",
+                  label: "View detail",
+                  uri: "http://example.com/page/123",
+                },
+              ],
+            },
           },
         ],
       });
@@ -39,21 +70,16 @@ router.post("/", async (req, res, next) => {
         body: dataString,
       };
 
-      // Define our request
       const request = https.request(webhookOptions, (res) => {
         res.on("data", (d) => {
           process.stdout.write(d);
         });
       });
 
-      // Handle error
-      // request.on() is a function that is called back if an error occurs
-      // while sending a request to the API server.
       request.on("error", (err) => {
         console.error(err);
       });
 
-      // Finally send the request and the data we defined
       request.write(dataString);
       request.end();
     }
