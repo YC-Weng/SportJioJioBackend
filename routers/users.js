6f2932d5ebcda83c158c062760149b49659efb20
yuchenweng7@gmail.com <yuchenweng7@gmail.com>
2024-03-21 21:07:34 +0800

    fix bugs

--- routers/users.js
+++ routers/users.js
@@ -57,6 +57,7 @@ router.post("/create", async (req, res, next) => {
     const { userName, userId, userPicUrl } = req.body;
     if (userName == null) res.send({ status: "fail" });
     else {
+      console.log(userPicUrl);
       console.log(
         `INSERT into users (id, name${
           userPicUrl == null ? ", pic_url" : ""
