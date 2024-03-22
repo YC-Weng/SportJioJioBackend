df93c25aab265efc051bfbff4e65bb4404272a62
yuchenweng7@gmail.com <yuchenweng7@gmail.com>
2024-03-22 14:28:39 +0800

    fix bugs

--- routers/line.js
+++ routers/line.js
@@ -5,14 +5,14 @@ const { pool } = require("../db");
 const { menu } = require("../uti");
 
 const TOKEN = process.env.LINE_TOKEN;
+const headers = {
+  "Content-Type": "application/json",
+  Authorization: "Bearer " + TOKEN,
+};
 
 const router = express.Router();
 
 const send_reply = (dataString) => {
-  const headers = {
-    "Content-Type": "application/json",
-    Authorization: "Bearer " + TOKEN,
-  };
   const webhookOptions = {
     hostname: "api.line.me",
     path: "/v2/bot/message/reply",
@@ -31,55 +31,67 @@ const send_reply = (dataString) => {
   request.end();
 };
 
+const gen_datastring = (replyToken, texts) => {
+  const msg = [];
+  for (t in texts) msg.push({ type: "text", text: t });
+  return JSON.stringify({
+    replyToken: replyToken,
+    messages: msg,
+  });
+};
+
+const get_group_member = (groupId) => {
+  const options = {
+    hostname: "api.line.me",
+    path: `/v2/bot/group/${groupId}/summary`,
+    method: "GET",
+    headers: headers,
+  };
+  https.request(options, (res) => {});
+};
+
 router.post("/", async (req, res, next) => {
   try {
     res.send("HTTP POST request sent to the webhook URL!");
+    console.log(`type: ${req.body.events[0].type}`);
     const replyToken = req.body.events[0].replyToken;
+    const reply_texts = [];
 
-    if (req.body.events[0].type === "message") {
+    if (req.body.events[0].type === "message" && req.body.events[0].message.type === "text") {
+      console.log(`text: ${req.body.events[0].message.text}`);
       if (req.body.events[0].message.text == "揪揪") {
-        const groupId =
-          req.body.events[0].source.groupId != null
-            ? req.body.events[0].source.groupId.slice(1)
-            : "";
-        console.log(groupId);
-        const dataString = JSON.stringify({
-          replyToken: replyToken,
-          messages: [menu(groupId)],
-        });
-        send_reply(dataString);
-      } else if (
-        req.body.events[0].message.text.startsWith("sjj setgroupname")
-      ) {
+        const groupId = req.body.events[0].source.groupId != null ? req.body.events[0].source.groupId.slice(1) : "";
+        send_reply(
+          JSON.stringify({
+            replyToken: replyToken,
+            messages: [menu(groupId)],
+          })
+        );
+      } else if (req.body.events[0].message.text.startsWith("sjj setgroupname")) {
         const name = req.body.events[0].message.text.split(" ")[2].slice(1, -1);
         try {
           await pool.query(
-            `UPDATE groups SET name = '${name}' WHERE id = '${req.body.events[0].source.groupId.slice(
-              1
-            )}'`
+            `UPDATE groups SET name = '${name}' WHERE id = '${req.body.events[0].source.groupId.slice(1)}'`
           );
-          dataString = JSON.stringify({
-            replyToken: replyToken,
-            messages: [{ type: "text", text: `已將群組名稱設置為 ${name} ` }],
-          });
+          reply_texts.push(`已將群組名稱設置為 ${name}`);
         } catch (err) {
           console.log(err);
-          dataString = JSON.stringify({
-            replyToken: replyToken,
-            messages: [{ type: "text", text: `無法更新群組名` }],
-          });
+          reply_texts.push(`無法更新群組名`);
         } finally {
-          send_reply(dataString);
+          send_reply(gen_datastring(replyToken, reply_texts));
         }
       }
-    } else if (
-      req.body.events[0].type === "join" &&
-      req.body.events[0].source.type === "group"
-    ) {
-      const groupId = req.body.events[0].source.groupId.slice(1);
-      await pool.query(
-        `INSERT INTO groups (id, name) values ('${groupId}', 'default')`
-      );
+    } else if (req.body.events[0].type === "join" && req.body.events[0].source.type === "group") {
+      const groupId = req.body.events[0].source.groupId;
+      try {
+        await pool.query(`INSERT INTO groups (id, name) values ('${groupId.slice(1)}', 'default')`);
+        reply_texts.push(`已建立群組`);
+      } catch (err) {
+        console.log(err);
+        reply_texts.push(`已建立群組`);
+      }
+      try {
+      } catch (err) {}
     }
   } catch (error) {
     console.log(error);
